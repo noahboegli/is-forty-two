6bc5b0faff40cd1c5ef703642e3b0bfefc6f8359
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2019-10-31 13:55:53 +0100

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# is-forty-two
-Check if a number is equal to 42.
+# Is Forty Two
+Is forty two is a powerful, well-engineered and optimised javascript library to check if a number is 42.
+The idea came from the library "is-thirteen" (https://github.com/jezen/is-thirteen) that does the same as this library but for the number 13.
+## Usage
+To start using this lovely library, simply include the script in your HTML document:
+```html
+<script src="is-forty-two.js"></script>
+```
+And then you can use it that way:
+```javascript
+is(85).dividedBy(2).times(4).plus(44).minus(2).multipleOf().FortyTwo(); // False
+is(1977).theYearTheAgeOfPeopleBornThisYearIs().FortyTwo(); // True
+```
+More generally:
+```javascript
+is(yourNumber).{operations}.{option}.FortyTwo();
+```
+Note: operations & options are not necessary:
+```javascript
+is(42).FortyTwo(); // True
+```
+
+### Operations
+* `minus(value)`
+* `plus(value)`
+* `times(value)`
+* `dividedBy(value)`
+
+### Options
+* `theYearTheAgeOfPeopleBornThisYearIs()`
+* `roughly()`
+* `multipleOf()`
---
+++ is-forty-two.js
@@ -0,0 +1,66 @@
+class FortyTwoNumberManager{
+    constructor(value){
+        this.value = value;
+        this.type = "strict";
+        return this;
+    }
+
+    minus(value){
+        this.value -= value;
+        return this;
+    }
+
+    plus(value){
+        this.value += value;
+        return this;
+    }
+
+    times(value){
+        this.value *= value;
+        return this;
+    }
+    
+    dividedBy(value){
+        this.value = this.value / value;
+        return this;
+    }
+
+    theYearTheAgeOfPeopleBornThisYearIs(){
+        this.value = new Date().getFullYear() - this.value;
+        return this;
+    }
+
+    roughly(){
+        this.type = "rough";
+        return this;
+    }
+
+    multipleOf(){
+        this.type = "multipleof";
+        return this;
+    }
+        
+
+    FortyTwo(){   
+        var isOk = false;   
+        if(this.type == "strict" && this.value % 42 == 0 && this.value - 42 == 0 && this.value == 42){
+            isOk = true;
+        }
+
+        if(this.type == "rough" && this.value > 41 && this.value < 43){
+            isOk = true;
+        }
+
+        if(this.type == "multipleof" && this.value % 42 == 0){
+            isOk = true;
+        }
+        return isOk;
+    }
+
+
+}
+
+
+function is(value){
+    return new FortyTwoNumberManager(value);
+}
